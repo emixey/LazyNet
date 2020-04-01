43a4e2903bb54d9a3ddaeba318e5ff90bdbdeb11
Егоров Михаил <ems@dekart.ru>
2020-04-01 12:34:42 +0300

    Изменил перечисление

--- LazyNet/Data/WeatherForecastService.cs
+++ LazyNet/Data/WeatherForecastService.cs
@@ -8,7 +8,7 @@ namespace LazyNet.Data
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Холодно", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
